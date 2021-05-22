dd69d9cb7f15e2244a1218cc2ffbfc7423454b1b
Husak Yurii <gusak.1881@gmail.com>
2021-05-22 18:39:57 +0200

    init develop branch

---
+++ generic-editor/src/views/Editor.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="editor"></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Editor",
+  components: {},
+});
+</script>
